c7b701cefe7567f156ca8ceee842dd934fb96113
Belen Torres <139027262+belentorresz@users.noreply.github.com>
2026-04-11 16:01:06 -0500

    Actividad

---
+++ UsoPreferencias/app/src/main/java/com/belen/usopreferencias/AppPreferenciasFragment.kt
@@ -0,0 +1,14 @@
+package com.belen.usopreferencias
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class AppPreferenciasFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+
+        setPreferencesFromResource(R.xml.misapppreferencias, rootKey)
+
+    }
+
+}
---
+++ UsoPreferencias/app/src/main/java/com/belen/usopreferencias/GaleriaActivity.kt
@@ -0,0 +1,43 @@
+package com.belen.usopreferencias
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class GaleriaActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+
+    private val imagenes = intArrayOf(
+        R.drawable.pic01, R.drawable.pic02,
+        R.drawable.pic03, R.drawable.pic04,
+        R.drawable.pic05, R.drawable.pic05,
+        R.drawable.pic05, R.drawable.pic05
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_galeria)
+
+        imageView = findViewById(R.id.imageView)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
+        recyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        val adapter = ImagenAdapter(imagenes) { position ->
+
+            imageView.setImageResource(imagenes[position])
+
+        }
+
+        recyclerView.adapter = adapter
+
+    }
+
+}
